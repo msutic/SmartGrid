5681b08e9ff8d56b83a5e733074cebf0af895556
Marko Sutic <msuticm@gmail.com>
2021-03-24 07:43:28 +0100

    refactor frontend

--- clientside/karma.conf.js
+++ clientside/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/smartgrid'),
+      dir: require('path').join(__dirname, './coverage/clientside'),
       subdir: '.',
       reporters: [
         { type: 'html' },
