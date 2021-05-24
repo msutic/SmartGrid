bb5c8816949ada29d9f75b00eb26d2ee208fdcf9
Marko Sutic <msuticm@gmail.com>
2021-05-24 20:42:22 +0200

    configured database

--- Core/SmartGrid/Models/Device.cs
+++ Core/SmartGrid/Models/Device.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,7 @@ namespace SmartGrid.Models
     public class Device
     {
         public string Type { get; set; }
-        [Key]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         public string Name { get; set; }
--- Core/SmartGrid/Startup.cs
+++ Core/SmartGrid/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,7 +30,7 @@ namespace SmartGrid
             services.AddControllers();
 
             services.AddDbContext<SmartGridContext>(options => 
-                    options(Configuration.GetConnectionString("SG_CS")));
+                    options.UseSqlServer(Configuration.GetConnectionString("SG_CS")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
