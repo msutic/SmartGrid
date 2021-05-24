7d88c3ffcaf584b56a8bb382fd79c2ccdf585d5e
Marko Sutic <msuticm@gmail.com>
2021-05-24 18:34:45 +0200

    updated packages

---
+++ Core/SmartGrid/Models/Device.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartGrid.Models
+{
+    public class Device
+    {
+        public string Type { get; set; }
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public double X_coordinate { get; set; }
+        public double Y_coordinate { get; set; }
+    }
+}
---
+++ Core/SmartGrid/Models/Incident.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartGrid.Models
+{
+    public class Incident
+    {
+        public int Id { get; set; }
+    }
+}
