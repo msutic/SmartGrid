f32b006e564a181e87377ba2b5da1205aef24692
Gvex98 <gvero.pr95.2017@uns.ac.rs>
2021-06-08 19:57:02 +0200

    Logout without button visibility toggle implemented

--- clientside/src/app/app.component.ts
+++ clientside/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TestAuthentificationServiceService } from './services/test-authentification-service.service';
+import {User} from '../app/entities/user';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clientside';
+  currentUser: User;
+  
+  isLoggedin:boolean=false;
+  constructor( private router:Router,private authenticationService:TestAuthentificationServiceService)
+  {
+    
+    this.authenticationService.currentUser.subscribe(x=>this.currentUser=x);
+
+  }
+  
+  logout()
+  {
+    alert("Logout pressed");
+    this.authenticationService.logout();
+    this.router.navigate(['/home']);
+  }
+
+  /*
+  isloggedin():boolean
+  {
+    if(!this.currentUser.username)
+    {
+      alert("User undefined");
+      return false;
+    }else
+    {
+      alert("User defined");
+      return true;
+    }
+  }
+  */
+  
 }
--- clientside/src/app/entities/user.ts
+++ clientside/src/app/entities/user.ts
@@ -18,4 +18,5 @@ export class User {
         this.password = password;
         
     }
+    
 }
